91168a8788d64f7d06898f0e45dbb78e37da12ac
esa <esasantriyas001@gmail.com>
2022-05-08 07:04:01 +0700

    firstx

--- app/index.js
+++ app/index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const fs = require('fs');
 const path = require('path')
-
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+app.use(requireHTTPS);
 // respond with "hello world" when a GET request is made to the homepage
 app.use('/',express.static('src/login'));
 app.get('/', (req, res) => {
